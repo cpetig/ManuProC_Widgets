9ba01c7404cbf097945caa3016ef6982f2cac77e
christof <christof@cvs.manuproc.berlios.de>
2005-12-21 07:24:43 +0000

    size requisition and positioning of the popup
    monotone christof@petig-baender.de 20 Dec 2005 14:52:11 +0000 f67a1254b30ea70a010c39bb0729d25270fee9a7

--- src/datewin_popup.cc
+++ src/datewin_popup.cc
@@ -1,9 +1,4 @@
-// generated 2005/12/19 18:03:59 CET by christof@vesta.petig-baender.de
-// using glademm V2.10.0.0
-//
-// newer (non customized) versions of this file go to datewin_popup.cc_new
-
-// This file is for your program, I won't touch it again!
+// 
 
 #include "config.h"
 #include "datewin_popup.hh"
@@ -31,4 +26,23 @@ datewin_popup::datewin_popup(datewin *p)
 { GtkWidget *toplevel = gtk_widget_get_toplevel (GTK_WIDGET(parent->gobj()));
   if (GTK_WIDGET_TOPLEVEL (toplevel))
     gtk_window_set_transient_for(gobj(),GTK_WINDOW(toplevel));
+  gint height, width, x, y;
+  parent->get_window()->get_origin(x,y);
+  gint dummyx,dummyy,dummyd;
+  parent->get_window()->get_geometry(dummyx,dummyy,width,height,dummyd);
+  gint avail_height = parent->get_screen()->get_height();
+  Gtk::Requisition mydimens = size_request();
+  y += height;
+  if (mydimens.width>width && x>=(width-mydimens.width))
+    x-= width-mydimens.width;
+  else if (width>mydimens.width) 
+    mydimens.width=width;
+  if (y + mydimens.height >= avail_height)
+    y -= height + mydimens.height;
+  move(x,y);
+  set_size_request(mydimens.width, mydimens.height);
+}
+
+datewin_popup::~datewin_popup()
+{ if (parent->popup==this) parent->popup=0;
 }
--- src/datewin_popup.hh
+++ src/datewin_popup.hh
@@ -1,13 +1,4 @@
-// generated 2005/12/19 18:03:59 CET by christof@vesta.petig-baender.de
-// using glademm V2.10.0.0
 //
-// newer (non customized) versions of this file go to datewin_popup.hh_new
-
-// you might replace
-//    class foo : public foo_glade { ... };
-// by
-//    typedef foo_glade foo;
-// if you didn't make any modifications to the widget
 
 #ifndef _DATEWIN_POPUP_HH
 #  include "datewin_popup_glade.hh"
@@ -23,5 +14,6 @@ class datewin_popup : public datewin_popup_glade
         void on_day_selected();
 public:
   datewin_popup(datewin *parent);
+  ~datewin_popup();
 };
 #endif
