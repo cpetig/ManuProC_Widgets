894c2cf8be9b30a90bd4aa251a2a014138430e01
christof <christof@cvs.manuproc.berlios.de>
2006-10-31 16:10:14 +0000

    myMessage viel vielseitiger monotone christof@petig-baender.de 01 Oct 2006 17:59:23 +0000 f46ff29353444d102187357a79674d6823312464

--- src/MyMessage.h
+++ src/MyMessage.h
@@ -5,35 +5,54 @@
 #include<Misc/SQLerror.h>
 #include <Misc/compiler_ports.h>
 
-class MyMessage : public Gtk::MessageDialog
+struct MyMessage : Gtk::MessageDialog
 {
-public:
+  std::string stringify(const SQLerror &e)
+  { char tmp[100]; 
+    std::string _msg;
+    snprintf(tmp,sizeof tmp,"DB-Error Code:%d %s\n",e.Code(),e.State().c_str());
+    _msg=tmp;
+    snprintf(tmp,sizeof tmp,"DB-Error Message:%s\n",e.Message().c_str());  
+    _msg+=tmp;
+    snprintf(tmp,sizeof tmp,"Context:%s\n",e.Context().c_str());  
+    _msg+=tmp;
+    return _msg;
+  }
+  std::string stringify(const ManuProC::Datumsfehler &e)
+  { std::strstream ostr;
+    ostr << e;  
+    return ostr.str();
+  }
 
- MyMessage() : Gtk::MessageDialog("") {}
+  MyMessage() : Gtk::MessageDialog("") {}
 
- MyMessage(const SQLerror &e) :
- 	 Gtk::MessageDialog("",Gtk::MESSAGE_ERROR)
- { set(e);
- }
+  MyMessage(const SQLerror &e,Gtk::MessageType mt=Gtk::MESSAGE_ERROR) 
+      : Gtk::MessageDialog(stringify(e),false,mt)
+  { }
  
- MyMessage(const ManuProC::Datumsfehler &e) :
- 	 Gtk::MessageDialog("",Gtk::MESSAGE_ERROR)
- { set(e);
- } 
+  MyMessage(const ManuProC::Datumsfehler &e,Gtk::MessageType mt=Gtk::MESSAGE_ERROR)
+      : Gtk::MessageDialog(stringify(e),false,mt)
+  { }
  
- MyMessage(const std::string &s,Gtk::MessageType mt=Gtk::MESSAGE_INFO) : 
- 		Gtk::MessageDialog(s,mt)
- {
- }
- 
- static int show_and_wait(const std::string &s, Gtk::Window *parent=0, Gtk::MessageType mt=Gtk::MESSAGE_INFO)
- { MyMessage m(s,mt);
-   if (parent) m.set_transient_for(*parent);
-   m.show();
-   return m.run();
- }
+  MyMessage(Glib::ustring const& s,Gtk::MessageType mt=Gtk::MESSAGE_INFO)
+      : Gtk::MessageDialog(s,false,mt)
+  { }
 
-public:
+  template <class T>
+   static int show_and_wait(T const& s, Gtk::Window *parent, Gtk::MessageType mt)
+  { MyMessage m(s,mt);
+    if (parent) m.set_transient_for(*parent);
+    m.show();
+    return m.run();
+  }
+  template <class T>
+   static int show_and_wait(T const& s, Gtk::Window *parent=0)
+  { MyMessage m(s);
+    if (parent) m.set_transient_for(*parent);
+    m.show();
+    return m.run();
+  }
+
  __deprecated void set_Message(const std::string msg) 
    { set_message(msg,true); 
    }
@@ -41,23 +60,10 @@ public:
    { set_message(msg,true); 
    }
  void set(const ManuProC::Datumsfehler &e)
-   {
-    std::strstream ostr;
-    ostr << e;  
-    set_message(ostr.str(),true);
+   { set_message(stringify(e),true);
    }
  void set(const SQLerror &e)
-   { 
-      char tmp[100]; 
-      std::string _msg;
-      
-      snprintf(tmp,sizeof tmp,"DB-Error Code:%d %s\n",e.Code(),e.State().c_str());
-      _msg=tmp;
-      snprintf(tmp,sizeof tmp,"DB-Error Message:%s\n",e.Message().c_str());  
-      _msg+=tmp;
-      snprintf(tmp,sizeof tmp,"Context:%s\n",e.Context().c_str());  
-      _msg+=tmp;
-      set_message(_msg,true);
+   { set_message(stringify(e),true);
    }
 };
 
