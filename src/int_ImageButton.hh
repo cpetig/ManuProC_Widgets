ee1398b7fdbcd4cd172eff527965e56407dcf8ec
petig <petig@stahlwille.de>
2010-05-19 08:56:57 +0000

    LGPL

--- src/int_ImageButton.hh
+++ src/int_ImageButton.hh
@@ -1,19 +1,19 @@
 // $Id: int_ImageButton.hh,v 1.2 2004/07/16 06:58:44 christof Exp $
 /*  libKomponenten: ManuProC's Widget library
- *  Copyright (C) 2004 Adolf Petig GmbH & Co. KG
+ *  Copyright (C) 2004-2010 Adolf Petig GmbH & Co. KG
  *  written by Christof Petig
  *
  *  This program is free software; you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
+ *  it under the terms of the GNU Lesser General Public License as published by
  *  the Free Software Foundation; either version 2 of the License, or
  *  (at your option) any later version.
  *
  *  This program is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
+ *  GNU Lesser General Public License for more details.
  *
- *  You should have received a copy of the GNU General Public License
+ *  You should have received a copy of the GNU Lesser General Public License
  *  along with this program; if not, write to the Free Software
  *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
  */
