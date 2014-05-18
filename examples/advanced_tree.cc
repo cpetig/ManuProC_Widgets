1ad9f6acc875a0fd2ba814a5b7eeec52276c49aa
petig <petig@stahlwille.de>
2014-05-18 16:55:59 +0000

    more complex example to test attributes

--- examples/advanced_tree.cc
+++ examples/advanced_tree.cc
@@ -13,22 +13,23 @@ struct MyRow: RowDataBase
   MyRow(unsigned _i=0) : i(_i) {}
   virtual cH_EntryValue Value(guint _seqnr,gpointer gp) const
   {
+    if (_seqnr==0) return cH_EntryValueIntString(i);
     return cH_EntryValueIntString(i==2 ? "big red" : (_seqnr+i)&1 ? "bold" : "italic");
-    //return cH_EntryValueIntString(i);
   }
   virtual std::string ToolTip(guint _seqnr,gpointer _g) const 
   { char buf[128]; snprintf(buf,sizeof(buf),"Tooltip for %d,%d", i, _seqnr); return buf; }
   // font rendering attributes
   virtual Pango::AttrList attributes(guint _seqnr,gpointer _g) const 
-  { return i==2 ? bigred : (_seqnr+i)&1 ? bold : italic; }
+  { return _seqnr==0 ? Pango::AttrList() : i==2 ? bigred : (_seqnr+i)&1 ? bold : italic; }
 };
 
 class MyProperties : public SimpleTreeModel_Properties
-{ virtual unsigned Columns() const { return 2; }
+{ virtual unsigned Columns() const { return 3; }
   virtual Glib::ustring Title(guint _seqnr) const
   { switch (_seqnr)
     { case 0: return "Integer";
       case 1: return "Second";
+      case 2: return "Third";
       default: return Glib::ustring();
     }
   }
@@ -53,13 +54,14 @@ int main(int argc, char **argv)
    
   Gtk::Window w;
   MyProperties prop;
-  SimpleTree s(2);
+  SimpleTree s(3);
   s.setProperties(prop,false);
   std::vector <cH_RowDataBase> datavec;
   datavec.push_back(new MyRow(1));
 //  datavec.push_back(new MyRow(1));
   datavec.push_back(new MyRow(2));
   datavec.push_back(new MyRow(3));
+  datavec.push_back(new MyRow(4));
   s.setDataVec(datavec);
   s.EnableTooltips(true);
   w.add(s);
